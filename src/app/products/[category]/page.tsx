85e89cf2abfef4d8bc0cb49e81a4a19dd0ac151c
Arjun Mohan <arjunpotter17@gmail.com>
2025-03-03 17:20:55 +0530

    fix: build

--- src/app/products/[category]/page.tsx
+++ src/app/products/[category]/page.tsx
@@ -1,5 +1,11 @@
+"use client";
 import ProductCategory from '@/pages/ProductCategory';
+import { useParams } from 'next/navigation';
 
-export default function ProductCategoryPage({ params }: { params: { category: string } }) {
-  return <ProductCategory category={params.category} />;
+export default function ProductCategoryPage() {
+  const params = useParams();
+  if (!params || !params.category) {
+    return <div>No category found</div>;
+  }
+  return <ProductCategory category={params.category as string} />;
 } 
